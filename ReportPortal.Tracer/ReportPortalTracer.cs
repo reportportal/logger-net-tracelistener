5745c3de72b1e37b482932b36220cdd57cadfc9a
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2016-10-06 23:15:26 +0300

    Added sources

---
+++ ReportPortal.Tracer/ReportPortalTracer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using ReportPortal.Client.Models;
+using ReportPortal.Client.Requests;
+using ReportPortal.Shared;
+
+namespace ReportPortal.Tracer
+{
+    /// <summary>
+    /// Custom trace listener for reporting logs directly to Report Portal.
+    /// Logs will be viewable under current test item from shared context.
+    /// </summary>
+    public class ReportPortalTracer: TraceListener
+    {
+        public override void Write(string message)
+        {
+            if (Bridge.Context.TestId != null)
+            {
+                var request = new AddLogItemRequest
+                {
+                    TestItemId = Bridge.Context.TestId,
+                    Level = LogLevel.Info,
+                    Time = DateTime.UtcNow,
+                    Text = message
+                };
+
+                Bridge.Service.AddLogItem(request);
+            }
+        }
+
+        public override void WriteLine(string message)
+        {
+            Write(message);
+        }
+    }
+}
