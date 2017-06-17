a1b5ecd2fa04c246af0caf08b76c47d6bf1022e9
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2017-06-17 21:55:22 +0300

    Use new formatted message

--- src/ReportPortal.Tracer/ReportPortalTracer.cs
+++ src/ReportPortal.Tracer/ReportPortalTracer.cs
@@ -10,19 +10,11 @@ namespace ReportPortal.Tracer
     /// Custom trace listener for reporting logs directly to Report Portal.
     /// Logs will be viewable under current test item from shared context.
     /// </summary>
-    public class ReportPortalTracer: TraceListener
+    public class ReportPortalTracer : TraceListener
     {
         public override void Write(string message)
         {
-            if (Bridge.Context.LaunchReporter != null && Bridge.Context.LaunchReporter.LastTestNode != null)
-            {
-                Bridge.Context.LaunchReporter.LastTestNode.Log(new AddLogItemRequest
-                {
-                    Level = LogLevel.Info,
-                    Time = DateTime.UtcNow,
-                    Text = message
-                });
-            }
+            Bridge.LogMessage(LogLevel.Info, message);
         }
 
         public override void WriteLine(string message)
